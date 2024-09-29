0e5811d18e6642761c17ce6d9e39b2bd4daaca04
Shree Vedha <24WH5A6601@bvrithyderabad.edu.in>
2024-09-29 14:46:28 +0530

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,29 @@
+class Book {  
+    String BookName;  
+    String isbnNumber;  
+    String AuthorName;  
+    String Publisher;  
+
+    public Book(String book, String isbn, String author, String publisher) {  
+        this.BookName = bookName;  
+        this.isbnNumber = isbnNumber;  
+        this.AuthorName = authorName;  
+        this.Publisher = publisher;  
+    }  
+
+    public String getBookInfo() {  
+        return "Book Name: " + BookName + "\n" +  
+               "ISBN: " + isbnNumber + "\n" +  
+               "Author: " + AuthorName + "\n" +  
+               "Publisher: " + Publisher;  
+    }  
+}  
+
+public class Main {  
+    public static void main(String[] args) {  
+        Book book = new Book(" Java", "14765498", "Robert C. ", "McGraw");  
+        System.out.println(book.getBookInfo());  
+
+     
+    }  
+}
